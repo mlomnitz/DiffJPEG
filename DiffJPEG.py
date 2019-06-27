4e0859e267b478eb52a2402d132ce773679b349a
Michael Lomnitz <mllomnitz@gmail.com>
2019-06-27 00:56:59 +0000

    Add DiffJPEG torch module

---
+++ DiffJPEG.py
@@ -0,0 +1,25 @@
+# Pytorch
+import torch
+import torch.nn as nn
+# Local
+from compression import compress_jpeg
+from decompression import decompress_jpeg
+from utils import diff_round, quality_to_factor
+
+
+class DiffJPEG(nn.Module):
+    def __init__(self, differentiable=True, quality=80):
+        super(DiffJPEG, self).__init__()
+        if differentiable:
+            self.rounding = diff_round
+        else:
+            self.rounding = torch.round
+        self.factor = quality_to_factor(quality)
+
+    def forward(self, x):
+        height, width = x.shape[2:]
+        y, cb, cr = compress_jpeg(
+            x, rounding=self.rounding, factor=self.factor)
+        recovered = decompress_jpeg(y, cb, cr, height=height, width=width,
+                                    rounding=self.rounding, factor=self.factor)
+        return recovered
