668491ec0afbffcf92723a684c1a30ce9504a7e6
Michael Lomnitz <mllomnitz@gmail.com>
2019-07-02 16:09:32 +0000

    Fixes DiffJPEG to load updated version frmo modules

--- DiffJPEG.py
+++ DiffJPEG.py
@@ -2,33 +2,34 @@
 import torch
 import torch.nn as nn
 # Local
-from compression import compress_jpeg
-from decompression import decompress_jpeg
+from modules import compress_jpeg, decompress_jpeg
 from utils import diff_round, quality_to_factor
 
 
 class DiffJPEG(nn.Module):
-    def __init__(self, differentiable=True, quality=80):
+    def __init__(self, height, width, differentiable=True, quality=80):
         ''' Initialize the DiffJPEG layer
         Inputs:
+            height(int): Original image hieght
+            width(int): Original image width
             differentiable(bool): If true uses custom differentiable
                 rounding function, if false uses standrard torch.round
             quality(float): Quality factor for jpeg compression scheme. 
         '''
         super(DiffJPEG, self).__init__()
         if differentiable:
-            self.rounding = diff_round
+            rounding = diff_round
         else:
-            self.rounding = torch.round
-        self.factor = quality_to_factor(quality)
+            rounding = torch.round
+        factor = quality_to_factor(quality)
+        self.compress = compress_jpeg(rounding=rounding, factor=factor)
+        self.decompress = decompress_jpeg(height, width, rounding=rounding,
+                                          factor=factor)
 
     def forward(self, x):
         '''
 
         '''
-        height, width = x.shape[2:]
-        y, cb, cr = compress_jpeg(
-            x, rounding=self.rounding, factor=self.factor)
-        recovered = decompress_jpeg(y, cb, cr, height=height, width=width,
-                                    rounding=self.rounding, factor=self.factor)
+        y, cb, cr = self.compress(x)
+        recovered = self.decompress(y, cb, cr)
         return recovered
